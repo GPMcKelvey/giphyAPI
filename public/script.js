9dbdb32d2719e780deece7afb9de92adf02093ea
GPMcKelvey <gpmckelvey@gmail.com>
2021-02-17 14:59:19 -0500

    fourth deploy

--- public/script.js
+++ public/script.js
@@ -19,7 +19,7 @@ fetch(`${baseURL}trending?api_key=${key}&limit=30&offset=5&rating=pg`)
     });
    
 function displayResults2(gif2){
-    //console.log(gif2);
+    console.log(gif2);
     for(let i = 0; i < gif2.data.length; i++){
     let div = document.createElement("div");
     if (i == 0){
@@ -28,7 +28,7 @@ function displayResults2(gif2){
     div.className = "carousel-item"};
     let img = document.createElement("img");
     img.className = "d-block w-100";
-    img.src = gif2.data[i].images.original.url;
+    img.src = gif2.data[i].images.fixed_height.url;
 
     carousel.appendChild(div);
     div.appendChild(img);
@@ -59,7 +59,7 @@ fetch(url)
 };
 
 function displayResults(gif){
-    //console.log(gif);
+    console.log(gif);
     for(let i = 0; i < gif.data.length; i++){
     let div = document.createElement("div");
     if (i == 0){
