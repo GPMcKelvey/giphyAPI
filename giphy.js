660fba48c6b92d6ffd665dd58c4433c93e0e1a0f
GPMcKelvey <gpmckelvey@gmail.com>
2021-02-16 21:08:17 -0500

    eleventh commit

--- giphy.js
+++ giphy.js
@@ -1,4 +1,4 @@
-const baseURL = 'https://api.giphy.com/v1/gifs/search';
+const baseURL = 'https://api.giphy.com/v1/gifs/';
 const key = 'tpSTAuQNc6cR4BKHDpvvsoOUAEDjv8eS';
 let url;
 
@@ -6,6 +6,35 @@ const searchTerm = document.querySelector(".search");
 const searchForm = document.querySelector("form");
 const submitBtn = document.querySelector("#submit");
 const carousel = document.querySelector(".carousel-inner");
+
+
+
+
+fetch(`${baseURL}trending?api_key=${key}&limit=30&offset=5&rating=pg`)
+    .then(response => response.json())
+    .then(json => {
+       
+        displayResults2(json); 
+        
+    });
+   
+function displayResults2(gif2){
+    //console.log(gif2);
+    for(let i = 0; i < gif2.data.length; i++){
+    let div = document.createElement("div");
+    if (i == 0){
+        div.className = "carousel-item active";
+    } else{
+    div.className = "carousel-item"};
+    let img = document.createElement("img");
+    img.className = "d-block w-100";
+    img.src = gif2.data[i].images.original.url;
+
+    carousel.appendChild(div);
+    div.appendChild(img);
+    };
+    
+};
 
 
 submitBtn.addEventListener("click", fetchResults);
@@ -16,7 +45,7 @@ function fetchResults(e){
     removeResults();
     e.preventDefault(); 
     const term = searchTerm.value.replace(' ', '');
-    url = `${baseURL}?api_key=${key}&q=${term}&limit=30&offset=5&rating=g&lang=en`;
+    url = `${baseURL}search?api_key=${key}&q=${term}&limit=30&offset=5&rating=pg&lang=en`;
     //console.log(url); 
 
 fetch(url)
@@ -30,7 +59,7 @@ fetch(url)
 };
 
 function displayResults(gif){
-    console.log(gif);
+    //console.log(gif);
     for(let i = 0; i < gif.data.length; i++){
     let div = document.createElement("div");
     if (i == 0){
